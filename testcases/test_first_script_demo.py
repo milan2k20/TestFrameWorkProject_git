6d2b9c771f63a03cc8d7109a0080418b6bcd8942
Om Prakash <73011776+milan2k20@users.noreply.github.com>
2021-12-14 23:02:10 +0530

    Update test_first_script_demo.py

--- testcases/test_first_script_demo.py
+++ testcases/test_first_script_demo.py
@@ -18,7 +18,7 @@ class TestSearchAndVerifyFilter(softest.TestCase):
 
 # Run testcase with multiple data by using tuple
 
-    '''@data(("GOI", "Mumbai", "10/01/2022", "20/02/2022", "Non Stop"), ("Chennai", "HYD", "14/01/2022", "25/02/2022", "Non Stop"))
+    @data(("GOI", "Mumbai", "10/01/2022", "20/02/2022", "Non Stop"), ("Chennai", "HYD", "14/01/2022", "25/02/2022", "Non Stop"))
     @unpack
     def test_search_flight(self, going_from, going_to, origin_date, return_date, stop):
         search_flight_result = self.search_flight_obj.search_flights(going_from, going_to, origin_date, return_date)
@@ -26,7 +26,7 @@ class TestSearchAndVerifyFilter(softest.TestCase):
         search_flight_result.filter_flights_by_stop(stop)
         all_stops = search_flight_result.get_search_flight_results()
         self.log.info(len(all_stops))
-        self.utils_obj.assert_list_item_text(all_stops, stop)'''
+        self.utils_obj.assert_list_item_text(all_stops, stop)
 
 # Run testcase with multiple data by using a json or yaml file
 
@@ -41,7 +41,7 @@ class TestSearchAndVerifyFilter(softest.TestCase):
 
     # Run testcase with multiple data by using Excel file
 
-    @data(*Utils.read_data_from_excelfile("E:\Selenium Project\TestFrameworkDemo\TestData\TestDataDemo.xlsx", "Sheet1"))
+    '''@data(*Utils.read_data_from_excelfile("E:\Selenium Project\TestFrameworkDemo\TestData\TestDataDemo.xlsx", "Sheet1"))
     @unpack
     def test_search_flight(self, going_from, going_to, origin_date, return_date, stop):
         search_flight_result = self.search_flight_obj.search_flights(going_from, going_to, origin_date, return_date)
@@ -49,7 +49,7 @@ class TestSearchAndVerifyFilter(softest.TestCase):
         search_flight_result.filter_flights_by_stop(stop)
         all_stops = search_flight_result.get_search_flight_results()
         self.log.info(len(all_stops))
-        self.utils_obj.assert_list_item_text(all_stops, stop)
+        self.utils_obj.assert_list_item_text(all_stops, stop)'''
 
 
 # Run testcase with multiple data by using CSV file
